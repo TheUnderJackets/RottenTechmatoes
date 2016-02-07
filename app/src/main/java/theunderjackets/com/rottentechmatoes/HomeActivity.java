e349447385ce67f909d27f191ebe01f917ea1ceb
Will Christian <wchristian6@gatech.edu>
2016-02-07 13:04:58 -0500

    Added Logout button & functions

--- app/src/main/java/theunderjackets/com/rottentechmatoes/HomeActivity.java
+++ app/src/main/java/theunderjackets/com/rottentechmatoes/HomeActivity.java
@@ -1,6 +1,7 @@
 package theunderjackets.com.rottentechmatoes;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,10 +11,20 @@ import android.view.View;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    SharedPreferences SM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        SM = getSharedPreferences("userrecord", 0);
+        Boolean isLogin = SM.getBoolean("userlogin", false);
+        if (isLogin) {
+            Intent intent = new Intent(HomeActivity.this, WelcomeScreenActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Intent intent = getIntent();
