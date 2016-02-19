141ddfdc1c69949868a4c70c89e9defae5bbc0d0
Andrew Suh <atsuh0124@gmail.com>
2016-02-18 23:26:57 -0500

    Stubbed out RottenTomatoes interface.

---
+++ app/src/main/java/theunderjackets/com/rottentechmatoes/RequestQueueSingleton.java
@@ -0,0 +1,66 @@
+package theunderjackets.com.rottentechmatoes;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.util.List;
+
+/**
+ * Created by Andrew Suh on 2/18/2016.
+ */
+public class RequestQueueSingleton {
+    private static RequestQueueSingleton queueInstance;
+    private RequestQueue requestQueue;
+    private List<Movie> resultsMovies;
+    private Movie resultsMovie;
+    private static Context cont;
+
+    private RequestQueueSingleton(Context context) {
+        cont = context;
+        requestQueue = getRequestQueue();
+    }
+
+    /**
+     * Singleton instance accessor
+     * @param context current context in application
+     * @return single instance of the class
+     */
+    public static RequestQueueSingleton getInstance(Context context) {
+        if (queueInstance == null) {
+            queueInstance = new RequestQueueSingleton(context);
+        }
+        return queueInstance;
+    }
+
+    /**
+     * Getter method for the request queue.
+     * @return request queue
+     */
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(cont.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    /**
+     * Add method for the request queue. For the purpose of this app, get a JsonObjectRequest
+     * from the RottenTomatoes class and add it to this queue.
+     * @param req
+     * @param <T>
+     */
+    public <T> void add(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public List<Movie> getMovies() {
+        return resultsMovies;
+    }
+
+    public Movie getMovie() {
+        return resultsMovie;
+    }
+}
--- app/src/main/java/theunderjackets/com/rottentechmatoes/RottenTomatoes.java
+++ app/src/main/java/theunderjackets/com/rottentechmatoes/RottenTomatoes.java
@@ -10,16 +10,80 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
+
 /**
  * Created by Andrew Suh on 2/18/2016.
  */
 public final class RottenTomatoes {
 
-    private static RequestQueue requestQueue;
+    private static String apiKey = "yedukp76ffytfuy24zsqk7f5";
 
     private RottenTomatoes() {
     }
 
+    /**
+     * Makes a REST request from RottenTomatoes and returns a JsonObjectRequest
+     * @param urlRequest the type of request in url form
+     * @param limit the limit on number of movies in the list (maximum)
+     * @param query the query of the search if of type keyword search
+     * @return the list of movies queried for
+     */
+    public static JsonObjectRequest getMovies(RottenTomatoesRequest urlRequest, int limit, String query) {
+    String url = urlRequest.getRequestURL() + "?";
+    if (urlRequest.equals(RottenTomatoesRequest.MOVIE_KEYWORD_SEARCH)) {
+        url += "q=" + query + "&";
+    }
+    url += "limit=" + limit;
+    url += "&country=us&apikey=" + apiKey;
+    return makeCall(url);
+    }
 
+    /**
+     * Makes a REST request from RottenTomatoes and gets a list of movies
+     * @param urlRequest the type of request in url form
+     * @param limit the limit on number of movies in the list (maximum)
+     * @return a request for the query made
+     */
+    public static JsonObjectRequest getMovies(RottenTomatoesRequest urlRequest, int limit) {
+        return getMovies(urlRequest, limit, null);
+    }
+    // Don't worry about this.
+    private static JsonObjectRequest makeCall(String url) {
+        return new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject resp) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+
+        });
+    }
+
+    /**
+     * Makes a REST request from RottenTomatoes and returns a JsonObjectRequest
+     * @param urlRequest enum request
+     * @param id id of the movie
+     * @return a request for the query made
+     */
+    public static JsonObjectRequest getMovieInfo(RottenTomatoesRequest urlRequest, String id) {
+        String url = urlRequest.getRequestURL() + "?";
+        url += "id=" + id;
+        url += "&country=us&apikey=" + apiKey;
+        return makeCall(url);
+    }
+
+    private void goToResultsScreen(List<Movie> movies) {
+
+    }
+
+    private void goToMovieScreen(Movie movie) {
+
+    }
 
 }
