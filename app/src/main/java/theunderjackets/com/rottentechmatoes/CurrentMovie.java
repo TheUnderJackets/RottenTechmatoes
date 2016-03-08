77d03ae4b73633f5382de2d0fefdb6934f128a73
masterhuddy <hudsonlynam@gatech.edu>
2016-03-07 23:09:29 -0500

    Added a Singleton for the current movie

---
+++ app/src/main/java/theunderjackets/com/rottentechmatoes/CurrentMovie.java
@@ -0,0 +1,39 @@
+package theunderjackets.com.rottentechmatoes;
+
+/**
+ * Created by Hudson Lynam on 3/7/2016.
+ */
+public class CurrentMovie {
+    private static CurrentMovie currentInstance = new CurrentMovie();
+    private Movie currentMovie;
+    private CurrentMovie() {
+    }
+
+    /**
+     * Getter method for the Singleton Pattern.
+     * @return the instance of CurrentMovie
+     */
+    public static CurrentMovie getInstance() {
+        return currentInstance;
+    }
+
+    /**
+     * Getter methpd for the current movie.
+     * @return the current movie
+     * @throws IllegalStateException if there is no current movie
+     */
+    public Movie getMovie() {
+        if (currentMovie == null) {
+            throw new IllegalStateException("There is no current movie.");
+        }
+        return currentMovie;
+    }
+
+    /**
+     * Setter method to set the current movie
+     * @param movie movie to set the currentMovie to
+     */
+    public void setMovie(Movie movie) {
+        currentMovie = movie;
+    }
+}
